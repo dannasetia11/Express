86b869f4d92e985025eba845639ca9d43369ba27
dannasetia <dannasetia@gmail.com>
2019-12-03 09:45:21 +0700

    add validate

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ message: "welcome to my api asd" });
+  res.send({ message: "welcome to my api" });
 });
 
 module.exports = router;
