91874a7c290c6c665cf23fa13a0f1bedebb994bc
dannasetia <dannasetia@gmail.com>
2019-12-04 16:39:15 +0700

    tambah email

--- routes/validate/login.js
+++ routes/validate/login.js
@@ -4,13 +4,6 @@ const { comparedPassword } = require("../../helpers");
 const login = async ({ email, password }) => {
   try {
     const result = {};
-    const data = await get()
-      .collection("users")
-      .findOne({ email: email })
-      .then(async result => {
-        const compared = await comparedPassword(password, result.password);
-        return compared;
-      });
 
     if (!email) {
       result.email = "Wajib Isi";
@@ -21,6 +14,14 @@ const login = async ({ email, password }) => {
     if (!password) {
       result.password = "Wajib Isi";
     } else if (password) {
+      const data = await get()
+        .collection("users")
+        .findOne({ email: email })
+        .then(async result => {
+          const compared = await comparedPassword(password, result.password);
+          return compared;
+        });
+
       if (!data) {
         result.password = "Email/Password salah";
       }
